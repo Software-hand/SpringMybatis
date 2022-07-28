a7ea7e4ce0e60112d7209cf095ab7e5892a2d9ae
软件手 <471583744@qq.com>
2022-07-29 00:48:11 +0800

    springboot-mybatis 相关操作

---
+++ src/test/java/com/rjs/SpringMybatis/SpringMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjs.SpringMybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
